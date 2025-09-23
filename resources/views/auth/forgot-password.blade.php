f4271e4e0f0f6d23473f91ebb337b3d860c76870
Juan David <juandavid690210@gmail.com>
2025-09-23 14:20:59 -0500

    Estilo agregado al formulario de recuperar contraseña

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -4,8 +4,8 @@
             <x-authentication-card-logo />
         </x-slot>
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        <div class="mb-4 text-sm text-black-600">
+            {{ __('Hola gamer olvidaste tu contraseña? No hay problema. Solo indícanos tu correo electrónico y te enviaremos un enlace para que puedas crear una nueva.') }}
         </div>
 
         @session('status')
@@ -16,19 +16,20 @@
 
         <x-validation-errors class="mb-4" />
 
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
+        <form method="POST" action="{{ route('password.email') }}" class="border border-gray-300 rounded-lg p-6 shadow-md bg-white">
+    @csrf
 
-            <div class="block">
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            </div>
+    <div class="block">
+        <x-label for="email" value="{{ __('Email') }}" />
+        <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+    </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-button>
-            </div>
-        </form>
+    <div class="flex items-center justify-end mt-4">
+        <x-button>
+            {{ __('Email Password Reset Link') }}
+        </x-button>
+    </div>
+</form>
+
     </x-authentication-card>
 </x-guest-layout>
